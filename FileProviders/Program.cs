7a736796cf8c19d249316b2eb758af9410480aed
Kautsky Lozano <kautskylozanov@outlook.com>
2019-09-04 05:51:12 +0200

    Add project files.

---
+++ FileProviders/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FileProviders
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
